75ead2fec4a35bb3b49e17c32e5ceefc2ec49367
Ricardo Teixeira <64698638+Ricardo-script@users.noreply.github.com>
2025-07-04 10:04:46 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ export default function App() {
     return (
         <main>
             <h1>Estudo de docker e ci/cd</h1>
+            <span>Run pipeline in github actions</span>
         </main>
     );
 }
